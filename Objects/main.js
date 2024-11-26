4cd67cec4cb70d980c3ab9378ec3c61a61719ed0
Michael Doughty <yekimcodes@gmail.com>
2024-11-26 07:21:34 -0400

    modified main.js file

--- Objects/main.js
+++ Objects/main.js
@@ -8,12 +8,12 @@ const person = {
     age: 43,
     // Objects methods
     bio() {
-        console.log(`${this.firstName} ${this.lastName} is ${this.age} years old.`);
+        console.log(`${this.name.firstName} ${this.name.middleName} ${this.name.lastName} is ${this.age} years old.`);
     },
     introduceSelf() {
-        console.log(`Hi! I'm ${this.firstName}.`);
+        console.log(`Hi! I'm ${this.name.firstName}.`);
     },
     firstInitial() {
-        console.log(`My first initial is ${this.firstName[0]}`);
+        console.log(`My first initial is ${this.name.firstName[0]}`);
     },
 };
