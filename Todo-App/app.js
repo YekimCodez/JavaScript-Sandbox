d543b84fe738d0855c2cb4460692116835df90a7
Michael Doughty <yekimcodes@gmail.com>
2024-11-29 05:35:48 -0400

    Made adjustments to the index.html and the main.scss

---
+++ Todo-App/app.js
@@ -0,0 +1,10 @@
+//Collect user input e.g, tasks
+const input = document.getElementById('text').value;
+const inputValue = document.getElementById("demo").value;
+
+
+const button = function () {
+    return document.getElementById('task-btn');
+}
+
+console.log(inputValue);
