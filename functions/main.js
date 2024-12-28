3dab8e7eabfe218f442d5f1f3a46c7ac19f1e282
Michael Doughty <yekimcodes@gmail.com>
2024-12-27 20:04:58 -0400

    Added variables, font family, sizes, weights etc, also resets.

--- functions/main.js
+++ functions/main.js
@@ -1,1 +1,12 @@
 // JavaScript Functions
+let msg = 'Sign up to receive our eekly newsletter for 10% off!';
+let updateMessage = function() {
+    let x = document.getElementById('message');
+    x.textContent = msg;
+}
+updateMessage();
+
+function sayHello() {
+    console.log('Hello');
+}
+sayHello();
