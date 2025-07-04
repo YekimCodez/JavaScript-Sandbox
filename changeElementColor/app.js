aa6922958ef3ca253f45d3fe09fccecab13bf294
Michael Doughty <yekim.codes@gmail.com>
2025-07-03 22:58:28 -0400

    I created a div and then created a div inside of it, created a paragraph element and appended it to the inner div and added an event listener to the inner div to change the bg color hen clicked

---
+++ changeElementColor/app.js
@@ -0,0 +1,30 @@
+// Get div element
+const container = document.querySelector('.container');
+
+// Create a new div element and append it to the container
+const newDiv = document.createElement('div');
+newDiv.classList.add('box');
+container.appendChild(newDiv);
+
+// Create a paragraph element and append it to the new div
+const paragraph = document.createElement('p');
+paragraph.textContent = 'This is a paragraph inside the new div.';
+paragraph.classList.add('text');
+paragraph.style.color = 'blue'; // Change text color to blue
+paragraph.style.textAlign = 'center'; // Center the text
+
+// Append the paragraph to the new div
+newDiv.appendChild(paragraph);
+
+//Append the container to the body
+document.body.appendChild(container);
+
+// Add an event listener to the new div
+newDiv.addEventListener('click', () => {
+    // Change the background color of the new div when clicked
+    if( newDiv.style.backgroundColor === 'lightgreen'){
+        newDiv.style.backgroundColor = 'lightcoral'; // Change to light coral
+    } else {
+        newDiv.style.backgroundColor = 'lightgreen'; // Change to light green
+    }   
+});
